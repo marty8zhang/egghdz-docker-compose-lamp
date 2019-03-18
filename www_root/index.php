dfdd7e6d6f0450828e0e1f1002dc890639b7ed08
Marty Zhang <marty8zhang@gmail.com>
2019-03-18 22:39:05 +1100

    Configured composer, added Xdebug and PHPMyAdmin.

--- www_root/index.php
+++ www_root/index.php
@@ -1,2 +1,36 @@
 <?php
-phpinfo();
+$dsn = 'mysql:dbname=Egghdz_Database;host=mariadb;charset=UTF8';
+$user = 'egghdz_user';
+$password = 'egghdzpassword';
+
+try {
+    $pdoDatabase = new PDO($dsn, $user, $password);
+
+    echo 'Connected to the database.<br>';
+
+    $pdoStatement = $pdoDatabase->query(<<<QUERYEND
+            CREATE TABLE IF NOT EXISTS test (
+                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                description TEXT
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8mb4_unicode_ci;
+            QUERYEND);
+
+    echo 'Created the `test` database table.<br>';
+
+    $uniqueId = uniqid();
+    $pdoStatement = $pdoDatabase->query(<<<QUERYEND
+            INSERT INTO test VALUES
+            (NULL, "Test Title {$uniqueId}", "Test description {$uniqueId}.");
+            QUERYEND);
+
+    echo 'Inserted '.$pdoStatement->columnCount(). ' record(s).<br>';
+
+    $pdoStatement = $pdoDatabase->query(<<<QUERYEND
+            SELECT * FROM test;
+            QUERYEND);
+
+    echo '<pre>'.print_r($pdoStatement->fetchAll(PDO::FETCH_ASSOC),true).'</pre>';
+} catch (PDOException $e) {
+    echo 'PDOException: ' . $e->getMessage();
+}
